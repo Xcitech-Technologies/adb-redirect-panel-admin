c7ae7dd3cb412f9891857eaf5a8ad382e35f1b48
vaibhav-xcitech <vaibhav.p@xcitech.in>
2022-05-25 17:27:53 +0530

    updated
    Tracking page-middle ege completed-update.

--- src/Components/Offers/Addoffer/Tracking.jsx
+++ src/Components/Offers/Addoffer/Tracking.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 import { BiAddToQueue } from "react-icons/bi";
+import jsonData from "../../../data.json";
+import Button from "../../../Controls/Button";
+
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
 const Tracking = () => {
+  const [ApproveIsOpen, setApproveIsOpen] = React.useState({
+    approveCountries: false,
+    approveTimeZone: false,
+    approveLanguage: false,
+  });
+
+  const arrayData = jsonData[0].MasterData.map((item) => ({
+    CountryCode: item.CountryCode,
+    Pop: item.Pop,
+    CountryName: item.CountryName,
+    Timezones: item.Timezones,
+    Language: item.Language,
+    CountryLanguagecode: item.CountryLanguagecode,
+    Domain: item.Domain,
+  }));
+  console.log(arrayData);
+
+  const handleOnSearch = (string, results) => {
+    console.log(string, results);
+  };
+
+  const handleOnSelect = (item) => {
+    console.log(item);
+    setApproveData([...approveData, item]);
+    setApproveIsOpen({
+      approveCountries: true,
+      approveTimeZone: true,
+      approveLanguage: true,
+    });
+  };
+
   const [addCondition, setAddCondition] = React.useState([
     { condition: "", open: true },
   ]);
@@ -23,6 +58,8 @@ const Tracking = () => {
   const addFieldOnclick = () => {
     setaddField([...addField, { condition: "" }]);
   };
+
+  const [approveData, setApproveData] = React.useState([]);
 
   return (
     <div className="container-fluid">
@@ -52,15 +89,76 @@ const Tracking = () => {
                   <div className="row">
                     <div className="col-md-6">
                       <div className="field-container form-group">
-                        <label>Country :</label>
-                        <input
-                          className="input"
-                          type="text"
-                          name=""
-                          id=""
-                          placeholder="Search Country Name"
-                        />
+                        <div className="autoSearch-label">
+                          <label style={{ marginTop: "15px" }}>Country :</label>
+                        </div>
+                        <div className="autoSearch">
+                          <ReactSearchAutocomplete
+                            items={arrayData}
+                            onSearch={handleOnSearch}
+                            onSelect={handleOnSelect}
+                            showIcon={false}
+                            placeholder="Search Country Name"
+                            styling={{
+                              height: "5px",
+                              backgroundColor: "#12141D",
+                              borderRadius: "5px",
+                              border: "none",
+                              color: "white",
+                              hoverBackgroundColor: "lightgreen",
+                              lineColor: "lightgreen",
+                            }}
+                            fuseOptions={{
+                              keys: ["CountryCode", "CountryName"],
+                            }} // Search on both fields
+                            resultStringKeyName="CountryName" // String to display in the results
+                          />
+                        </div>
                       </div>
+                      {ApproveIsOpen.approveCountries && (
+                        <div className="collapse-container form-group">
+                          <input
+                            defaultChecked 
+                            className="radio-input"
+                            type="radio"
+                            name="country"
+                            id="include"
+                          />
+                          <label className="radio-label" htmlFor="include">
+                            Include
+                          </label>
+                          <input
+                            className="radio-input"
+                            type="radio"
+                            name="country"
+                            id="exclude"
+                          />
+                          <label className="radio-label" htmlFor="exclude">
+                            Exclude
+                          </label>
+                          <div className="clear-btn">
+                            <Button
+                              onClick={() => {
+                                setApproveIsOpen({
+                                  approveCountries: false,
+                                });
+                                setApproveData([]);
+                              }}
+                            >
+                              Clear
+                            </Button>
+                          </div>
+                          {approveData.map((item, index) => {
+                            console.log(item);
+                            return (
+                              <span className="api-item" key={index}>
+                                <span className="x">x</span>
+                                {item.CountryName}
+                              </span>
+                            );
+                          })}
+                        </div>
+                      )}
                     </div>
 
                     <div className="col-md-6">
@@ -71,7 +169,7 @@ const Tracking = () => {
                           type="text"
                           name=""
                           id=""
-                          placeholder="Search Region Name"
+                          placeholder="Enter Region Name"
                         />
                       </div>
                     </div>
@@ -86,8 +184,9 @@ const Tracking = () => {
                           type="text"
                           name=""
                           id=""
-                          placeholder="Search Cities Name"
+                          placeholder="Enter Cities Name"
                         />
+                        
                       </div>
                     </div>
 
@@ -99,7 +198,7 @@ const Tracking = () => {
                           type="text"
                           name=""
                           id=""
-                          placeholder="Search Device Name"
+                          placeholder="Enter Device Name"
                         />
                       </div>
                     </div>
@@ -108,10 +207,26 @@ const Tracking = () => {
                   <div className="row">
                     <div className="col-md-12">
                       <div className="radio-container form-group">
-                        <input className="radio-input" selected type="radio" name="redio" id="1" />
-                        <label className="radio-label" for="1">Weight Base</label>
-                        <input className="radio-input" type="radio" name="redio" id="2" />
-                        <label className="radio-label" for="2">Time Base</label>
+                        <input
+                          defaultChecked 
+                          className="radio-input"
+                          selected
+                          type="radio"
+                          name="radio"
+                          id="1"
+                        />
+                        <label className="radio-label" htmlFor="1">
+                          Weight Base
+                        </label>
+                        <input
+                          className="radio-input"
+                          type="radio"
+                          name="radio"
+                          id="2"
+                        />
+                        <label className="radio-label" htmlFor="2">
+                          Time Base
+                        </label>
                       </div>
                     </div>
                   </div>
@@ -157,13 +272,25 @@ const Tracking = () => {
                     <div className="col-md-12">
                       <div className="pt-container form-group">
                         <label>PT URLs :</label>
-                        <input className="pt-input" type="text" placeholder="Desktop PT URL"/>
+                        <input
+                          className="pt-input"
+                          type="text"
+                          placeholder="Desktop PT URL"
+                        />
                       </div>
                       <div className="form-group">
-                        <input className="pturl-input" type="text" placeholder="Mobile PT URL"/>
+                        <input
+                          className="pturl-input"
+                          type="text"
+                          placeholder="Mobile PT URL"
+                        />
                       </div>
                       <div className="form-group">
-                        <input className="pturl-input" type="text" placeholder="intermediary URL"/>
+                        <input
+                          className="pturl-input"
+                          type="text"
+                          placeholder="intermediary URL"
+                        />
                       </div>
                     </div>
                   </div>
@@ -179,6 +306,39 @@ const Tracking = () => {
                           id=""
                           placeholder="Search Country Name"
                         />
+                        {ApproveIsOpen.approveTimeZone && (
+                          <div className="collase">
+                            <Button
+                              onClick={() => {
+                                setApproveIsOpen({
+                                  ...ApproveIsOpen,
+                                  approveTimeZone: false,
+                                });
+                                setApproveData([]);
+                              }}
+                            >
+                              Clear
+                            </Button>
+                            <div className="display-data">
+                              {approveData.map((item) => {
+                                const approveTimezone =
+                                  item.Timezones.split(",");
+                                return (
+                                  <div>
+                                    {approveTimezone.map((item, index) => {
+                                      return (
+                                        <span className="api-item" key={index}>
+                                          <span className="x">x</span>
+                                          {item}
+                                        </span>
+                                      );
+                                    })}
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
 
@@ -192,12 +352,42 @@ const Tracking = () => {
                           id=""
                           placeholder="Search Region Name"
                         />
+                        {ApproveIsOpen.approveLanguage && (
+                          <div className="collase">
+                            <Button
+                              onClick={() => {
+                                setApproveIsOpen({
+                                  ...ApproveIsOpen,
+                                  approveLanguage: false,
+                                });
+                                setApproveData([]);
+                              }}
+                            >
+                              Clear
+                            </Button>
+                            <div className="display-data">
+                              {approveData.map((item) => {
+                                const approveLanguage =
+                                  item.Language.split(",");
+                                return (
+                                  <div>
+                                    {approveLanguage.map((item, index) => {
+                                      return (
+                                        <span className="api-item" key={index}>
+                                          <span className="x">x</span>
+                                          {item}
+                                        </span>
+                                      );
+                                    })}
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
-
-                  
-
                 </div>
               )}
             </div>
